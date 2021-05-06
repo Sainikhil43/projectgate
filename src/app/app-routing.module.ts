3198c236d00e2d12f9a1a44b70ba285b52b7f7f1
sainikhil <raghupatruninikhil@gmail.com>
2021-05-05 19:38:17 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcComponent } from './ac/ac.component';
+import { CaoComponent } from './cao/cao.component';
+import { CivilComponent } from './civil/civil.component';
+import { CnComponent } from './cn/cn.component';
+import { CseComponent } from './cse/cse.component';
+import { DcComponent } from './dc/dc.component';
+import { DigitallogicComponent } from './digitallogic/digitallogic.component';
+import { EceComponent } from './ece/ece.component';
+import { EdcComponent } from './edc/edc.component';
+import { EeeComponent } from './eee/eee.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MechComponent } from './mech/mech.component';
+import { NetworksComponent } from './networks/networks.component';
+import { OsComponent } from './os/os.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"HOME",component:HomeComponent},
+  {path:"cse",component:CseComponent,
+  children:[
+    {path:"cao",component:CaoComponent},
+    {path:"os",component:OsComponent},
+    {path:"cn",component:CnComponent}
+  ]},
+  {path:"ece",component:EceComponent,
+  children:[
+    {path:"edc",component:EdcComponent},
+    {path:"ac",component:AcComponent},
+    {path:"dc",component:DcComponent},
+    {path:"networks",component:NetworksComponent}
+  ]},
+  {path:"eee",component:EeeComponent},
+  {path:"mech",component:MechComponent},
+  {path:"civil",component:CivilComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path: '**',component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EceComponent } from './ece/ece.component';
+import { EeeComponent } from './eee/eee.component';
+import { MechComponent } from './mech/mech.component';
+import { CivilComponent } from './civil/civil.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DigitallogicComponent } from './digitallogic/digitallogic.component';
+import { CaoComponent } from './cao/cao.component';
+import { OsComponent } from './os/os.component';
+import { CnComponent } from './cn/cn.component';
+import { CseComponent } from './cse/cse.component';
+import { EdcComponent } from './edc/edc.component';
+import { AcComponent } from './ac/ac.component';
+import { DcComponent } from './dc/dc.component';
+import { NetworksComponent } from './networks/networks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EceComponent,
+    EeeComponent,
+    MechComponent,
+    CivilComponent,
+    LoginComponent,
+    RegisterComponent,
+    DigitallogicComponent,
+    CaoComponent,
+    OsComponent,
+    CnComponent,
+    CseComponent,
+    EdcComponent,
+    AcComponent,
+    DcComponent,
+    NetworksComponent
   ],
   imports: [
     BrowserModule,
